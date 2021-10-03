43282ccc4734f243608ec8d955788ae7414ec9e9
YoukiRinRin <72230296+YoukiRinRin@users.noreply.github.com>
2021-10-03 16:21:29 +0900

    Add files via upload

---
+++ BS/bs.php
@@ -0,0 +1,21 @@
+
+<html>
+    
+</html>
+
+<?php
+
+$genkin = $_POST["genkin"];
+$uriage = $_POST["uriage"];
+$shohin = $_POST["shohin"];
+$bihin = $_POST["bihin"];
+$kaikake = $_POST["kaikake"];
+$kariire = $_POST["kariire"];
+$kurikosi = $_POST["kurikosi"];
+
+
+
+
+
+
+?>
