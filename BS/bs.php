eb45e2df20fcb9d06d9a7816520f3f722598efac
YoukiRinRin <72230296+YoukiRinRin@users.noreply.github.com>
2021-10-05 19:27:50 +0900

    Add files via upload

--- BS/bs.php
+++ BS/bs.php
@@ -1,6 +1,73 @@
-
+<head>
+    <link rel = "stylesheet" href = "style.css">
+   
+</head>
 <html>
+
+<div class ="BS">
+    <ul>バランスシート
+        <div class = "column">
+            <ul>資産
+                <li>a<li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+            </ul>    
+            <ul>金額
+                <li>a<li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+            </ul>    
+            <ul>負債
+                <li>a<li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+            </ul>
+            <ul>金額
+                <li>a<li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+                <li><li>
+            </ul> 
+    </ul>
+</div>
+
+<table>
+    <tr>
+        <th>資産</th> <th>金額</th> <th>負債</th> <th>金額</th>
+    </tr>
     
+    <tr>
+        <td></td><td></td><td></td><td></td><td></td>
+    </tr>
+
+    <tr>
+        <td></td><td></td><td></td><td></td><td></td>
+    </tr>
+
+    <tr>
+        <td></td><td></td><td></td><td></td><td></td>
+    </tr>
+
+    <tr>
+        <td></td><td></td><td></td><td></td><td></td>
+    </tr>
+
+</table>
+
+
+
+
 </html>
 
 <?php
